e216e53deb29e4aaf36a0b9338416d937b414292
Kamran Javid <kj316@cam.ac.uk>
2018-10-28 12:31:10 +0000

    Update test_example.cpp

--- forward_models/cpp_models/src/test/test_example.cpp
+++ forward_models/cpp_models/src/test/test_example.cpp
@@ -5,8 +5,6 @@
 
 TEST_CASE("example_function","[example][function]") 
 {
-    REQUIRE( example_function(3) == 10 );
-    REQUIRE( example_function(2) == 8 );
-    REQUIRE( example_function(-1) == 2 );
-    REQUIRE( example_function(0) == 4 );
+    //REQUIRE( example_function(3) == 10 );
+   
 }
