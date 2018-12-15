d33f82ab81d6e2f02b9fcadd6a2e65fae427c413
Kamran Javid <kj316@cam.ac.uk>
2018-12-15 11:29:25 +0000

    updated bnn predictor

--- forward_models/python_models/bnn_predictions.py
+++ forward_models/python_models/bnn_predictions.py
@@ -75,40 +75,72 @@ class bnn_predictor():
 
 	def y_pred_chains(self, file = None, test_index = 0):
 		"""
-		note LL values here are LLs from weight posterior,
-		not pred posterior.
+		essentially calculates p(f(theta, x)| X, Y) = p(y_pred(theta) | x, X, y) where theta are the nn parameters,
+		f(.) is the function mapping from input x to output y (i.e. the nn and f(theta, x) = y_pred(theta)), x is the input
+		data, y is true the output, y_pred is the predicted output, X and Y are the x and y training data respectively.
+		note posterior weight and LL values here are from weight posterior.
 		assumes self.x has shape (m,n) or (m,), not (n,)
-		TODO: add option to include LLs from pred posterior instead
-		of LLs from training,
-		by evaluating LL of real y given pred y, x, weights.
 		"""
 		try:
 			x = self.x[test_index, :].reshape(1,-1)
 		except KeyError:
-			x = self.x[test_index].reshape(1,-1)
+			x = self.x[test_index].reshape(1,-1)	
+		#get predictions of y from nn using each set of weights in posterior
+		pred = self.get_pred_arr(x)
+		pred_chains_arr = np.concatenate([self.posterior_weights.reshape(-1,1), self.LL.reshape(-1,1), pred], axis = 1)
+		if file:
+			np.savetxt(file, pred_chains_arr)
+		return pred_chains_arr
+
+	def y_test_prob(self, test_index = 0):
+		"""
+		n.b. need to call setup_LL before using this method, as it needs to calculate
+		test likelihood values.
+		returns single probability, as nn parameters are marginalised over:
+		p(y_test | x_test, x_train, y_train) = sum_i p(y_test | y_pred(theta_i), x_test, x_train, y_train) p(y_pred(theta_i) | x_test, x_train, y_train)
+		"""
+		try:
+			x = self.x[test_index, :].reshape(1,-1)
+		except KeyError:
+			x = self.x[test_index].reshape(1,-1)	
 		try:
 			y_true = self.y[test_index, :].reshape(1,-1)
 		except KeyError:
 			y_true = self.y[test_index].reshape(1,-1)
-		#definitely not most efficient way of doing this, using model.evaluate and model.predict,
+		#definitely not most efficient way of doing this, using both model.evaluate and model.predict,
 		#but efficiency here not particularly important, and just stole methods from keras_forward, so it will do
-		pred_LL = self.get_pred_LL(x, y_true)
-		pred = self.get_pred_arr(x)
-		pred_chains_arr = np.concatenate([self.posterior_weights.reshape(-1,1), self.LL.reshape(-1,1), pred], axis = 1)
+		test_LL = self.get_test_LL(x, y_true)
+		y_test_prob = np.sum(test_LL * self.posterior_weights)
+		return y_test_prob
 
-	def get_pred_LL(x, y):
-		preds = np.zeros((len(self.posterior_weights), 1))
+	def y_custom_prob(self, y):
+		"""
+		as y_test_prob(), but takes y as argument, so don't have to use true value
+		"""
+		LL = self.get_test_LL(x, y)
+		y_prob = np.sum(LL * self.posterior_weights)
+		return y_prob
+
+	def get_test_LL(x, y):
+		"""
+		calculate log-likelihood LL(y_test | y_pred(weights), x_test)
+		for all y_pred/weights from weights posterior file
+		"""
+		test_LL = np.zeros_like(self.posterior_weights)
 		for i in range(len(self.posterior_weights)):
 			self.set_k_weights(self.nn_param_sets[i])
-			pred_LL[i] = self.LL(x, y)
-		return pred_LL
+			test_LL[i] = self.LL(x, y)
+		return test_LL
 
 	def get_pred_arr(x):
-		preds = np.zeros((len(self.posterior_weights), y.shape()[1]))
+		"""
+		get set of predictions (vectors) for each set of nn parameters
+		"""
+		pred = np.zeros((len(self.posterior_weights), y.shape()[1]))
 		for i in range(len(self.posterior_weights)):
 			self.set_k_weights(self.nn_param_sets[i])
-			preds[i] = self.model.predict(x)
-		return preds
+			pred[i] = self.model.predict(x)
+		return pred
 
 	def setup_LL(self, loss):
         """
